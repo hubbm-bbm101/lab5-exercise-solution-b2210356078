e682addb7a95e15332048a0404685dea967c47f3
b2210356078 <92469185+b2210356078@users.noreply.github.com>
2021-11-05 15:31:26 +0300

    Update exercise5,2.py

--- b2210356078/exercise5,2.py
+++ b2210356078/exercise5,2.py
@@ -4,7 +4,7 @@ def isitemail(m):
     else:
         return False        
 email = input("please enter a thing that you want it to check e-mail or not:")
-if isitemail==True:
+if isitemail(email)==True:
     print("That is a e-mail.")
 else:
     print("That is not a e-mail.")
