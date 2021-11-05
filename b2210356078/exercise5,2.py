22d1e3c585f09105ae54a878b12e865525a39b22
b2210356078 <92469185+b2210356078@users.noreply.github.com>
2021-11-05 15:29:58 +0300

    Update exercise5,2.py

--- b2210356078/exercise5,2.py
+++ b2210356078/exercise5,2.py
@@ -1,6 +1,10 @@
-mail= input("please enter a thing that you want it to check e-mail or not:")
-x = "@" and "." in mail
-if x==True:
+def isitemail(m):
+    if "@" and "." in m:
+        return True
+    else:
+        return False        
+email = input("please enter a thing that you want it to check e-mail or not:")
+if isitemail==True:
     print("That is a e-mail.")
 else:
     print("That is not a e-mail.")
